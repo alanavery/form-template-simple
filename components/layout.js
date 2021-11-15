979de0b83d4c501dd713085baa6bda4f9a8482ad
Alan Avery <alan.avery@upshotmail.com>
2021-11-14 18:04:05 -0600

    Add title element and lang attribute

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,12 @@
 import Header from './Header';
+import Head from 'next/head'
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Form Templates</title>
+      </Head>
       <Header />
       <main>{children}</main>
     </>
